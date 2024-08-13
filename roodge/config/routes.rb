f810458aadc0d4e16550e0e33347395304bf5d9b
Giux <giuxeppe.marchioo@gmail.com>
2024-08-13 09:09:42 +0200

    files updated

--- roodge/config/routes.rb
+++ roodge/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  #GET /about
+  get "about", to: "about#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
