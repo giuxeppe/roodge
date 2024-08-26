65f2335465dd03459ac9eaa7d0216f1940e8ca7b
Giux <giuxeppe.marchioo@gmail.com>
2024-08-26 16:38:26 +0200

    database login

--- roodge/app/models/commenti.rb
+++ roodge/app/models/commenti.rb
@@ -1,4 +1,4 @@
 class Commenti < ApplicationRecord
-  validates :codice, :room, :proprietario, :titolo presence: true
+  validates :codice, :room, :proprietario, :titolo, presence: true
   validates :codice, uniqueness: { scope: [:room, :proprietario, :titolo] }
 end
--- roodge/config/routes.rb
+++ roodge/config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
+  #get 'registrations/new'
+  #get 'registrations/create'
+  #get 'registrations/edit'
+  #get 'registrations/update'
+  #get 'home/index'
   #GET /about
   get "about-us", to: "about#index", as: :about
   get "home/home_logged", to: "home#home_logged"
   get "registration", to: "home#registration"
+  get "final_registration", to: "home#final_registration"
   get "home/final_registration", to: "home#final_registration"
 
   root "home#index"
+
+  resources :registrations, only: [:new, :create] do
+    member do
+      get 'edit_final', to: 'home#final_registration'
+      patch 'update_final', to: 'registrations#update_final'
+    end
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
