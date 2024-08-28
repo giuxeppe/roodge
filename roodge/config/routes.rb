dd6d3c27c7351bbd35c93bc154b1a4cf824791b2
Giux <giuxeppe.marchioo@gmail.com>
2024-08-28 10:07:31 +0200

    modifiche database

--- roodge/config/routes.rb
+++ roodge/config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
   get "home/home_logged", to: "home#home_logged"
   get "registration", to: "home#registration"
-  get "final_registration", to: "home#final_registration"
-  get "home/final_registration", to: "home#final_registration"
   get "info_professor", to: "home#info_professor"
   root "home#index"
 
