f0f99f62a066b18752b09ba19a89966d65be3423
Riccardo Mantegazza <riccardomante@gmail.com>
2024-08-28 10:03:14 +0200

    info students

--- roodge/config/routes.rb
+++ roodge/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "registration", to: "home#registration"
   get "home/final_registration", to: "home#final_registration"
   get "info_professor", to: "home#info_professor"
+  get "info_student", to: "home#info_student"
   root "home#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
