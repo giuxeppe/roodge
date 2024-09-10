afeb66068ab32de005d5eb091c33005d97c47d1b
NoSql1 <maiucci.alessio02@gmail.com>
2024-09-10 18:42:30 +0200

    salvataggio database tag docente

--- roodge/app/controllers/registrations_controller.rb
+++ roodge/app/controllers/registrations_controller.rb
@@ -6,6 +6,10 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # Se l'utente è un docente, salva i tag associati
+      if @user.ruolo == 'd'
+        save_tags_for_docente(@user)
+      end
       redirect_to root_path, notice: 'Registrazione completata con successo!'
     else
       flash.now[:alert] = 'Errore: uno o più campi non sono stati compilati correttamente'
@@ -14,6 +18,20 @@ class RegistrationsController < ApplicationController
   end
 
   private
+
+   # Metodo per salvare i tag per il docente
+   def save_tags_for_docente(user)
+    tag1 = params[:user][:tag1]
+    tag2 = params[:user][:tag2]
+    tag3 = params[:user][:tag3]
+
+    # Creiamo una riga nella tabella tag_docentes per ogni tag selezionato
+    [tag1, tag2, tag3].each do |tag|
+      if tag.present?
+        TagDocente.create(docente: user.nome_utente, tag: tag)
+      end
+    end
+  end
 
   def user_params
     params.require(:user).permit(:nome_utente, :nome, :cognome, :cf, :data_nascita, :citta_nascita, :indirizzo_residenza, :mail, :ruolo, :propic, :bio, :password, :password_confirmation)
