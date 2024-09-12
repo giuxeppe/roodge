8bcb7183eb92db250c60620510b259eabe5e80f2
Giux <giuxeppe.marchioo@gmail.com>
2024-09-12 18:02:41 +0200

    Add Commenti FINITI

--- roodge/db/schema.rb
+++ roodge/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_12_103128) do
+ActiveRecord::Schema.define(version: 2024_09_12_154759) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2024_09_12_103128) do
     t.string "testo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "materiale"
   end
 
   create_table "cronologia_visitates", force: :cascade do |t|
@@ -95,7 +96,7 @@ ActiveRecord::Schema.define(version: 2024_09_12_103128) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
-  end 
+  end
 
   create_table "studente_rooms", force: :cascade do |t|
     t.string "studente", null: false
